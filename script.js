0bc377b8c18906cc6cfc6aa4bd8233bc0b27967e
dariapolkhovski <dashaxd14@gmail.com>
2024-10-12 15:04:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
-let categories = ["Напитки", "Популярная еда в России", "Сети быстрого питания", "Сладости", "Специи, приятности, приправы", "Фрукты, ягоды, овощи", "Что в холодильнике", "Завтра будет...", "Лучшие друзья никогда не...", "Мой начальник ...", "Моя собака...", "Пахнет как...", "Я чувствую себя..", "Известные кинофранзы", "Мультфильмы и мультсериалы", "Реалити шоу", "Фильмы", "Сериалы", "Города", "Известные ученые и изобретатели", "Исторические люди", "Минералы, драгоценные камни, камни и т.д", "Достопримечательности", "Столицы", "Страны", "Типично русские вещи", "Школьные предметы и занятые", "Языки", "Вещи из космоса", "Млекопитающие", "На что бывают аллергии", "Насекомые, рептилии, и прочее", "Птицы", "Рыбы", "Цветы", "Части человеческого тела", "Элементы периодической таблицы", "Музыкальные инструменты", "Актеры и актрисы", "Музыкальные исполнители и группы", "Стили музыки", "Все для дня рождения", "Из чего сделаны предметы", "Инструменты", "Марки автомобилей", "Мебель", "Места, куда я хотел бы пойти", "Офисные и школьные принадлежности", "Профессии", "Типы транспорта", "Холодные вещи", "Электроника и гаджеты", "Женские имена", "Мужские имена", "Слова с буквами «ор» в конце", "Слова с буквами «ст» в конце", "Слова с буквой «ы»", "Слова с буквой «ь»", "Слова с удвоенными согласными", "Виды спорта", "Вещи в ванной", "Вещи которые вешают на стену", "Вещи которые кладут в карман", "Вещи, которые обычно зеленые", "Вещи, которые обычно красные", "Детские вещи", "Круглые вещи", "Липкие вещи", "Любовь - какая?", "Романтические вещи", "Свадебные вещи"];
+let categories = [
+    "Напитки", "Популярная еда в России", "Сети быстрого питания", "Сладости",
+    "Специи, приятности, приправы", "Фрукты, ягоды, овощи", "Что в холодильнике",
+    "Завтра будет...", "Лучшие друзья никогда не...", "Мой начальник ...",
+    "Моя собака...", "Пахнет как...", "Я чувствую себя..", "Известные кинофранзы",
+    "Мультфильмы и мультсериалы", "Реалити шоу", "Фильмы", "Сериалы", "Города",
+    "Известные ученые и изобретатели", "Исторические люди",
+    "Минералы, драгоценные камни, камни и т.д", "Достопримечательности", "Столицы",
+    "Страны", "Типично русские вещи", "Школьные предметы и занятые", "Языки",
+    "Вещи из космоса", "Млекопитающие", "На что бывают аллергии",
+    "Насекомые, рептилии, и прочее", "Птицы", "Рыбы", "Цветы",
+    "Части человеческого тела", "Элементы периодической таблицы", "Музыкальные инструменты",
+    "Актеры и актрисы", "Музыкальные исполнители и группы", "Стили музыки",
+    "Все для дня рождения", "Из чего сделаны предметы", "Инструменты", "Марки автомобилей",
+    "Мебель", "Места, куда я хотел бы пойти", "Офисные и школьные принадлежности",
+    "Профессии", "Типы транспорта", "Холодные вещи", "Электроника и гаджеты",
+    "Женские имена", "Мужские имена", "Слова с буквами «ор» в конце",
+    "Слова с буквами «ст» в конце", "Слова с буквой «ы»", "Слова с буквой «ь»",
+    "Слова с удвоенными согласными", "Виды спорта", "Вещи в ванной",
+    "Вещи которые вешают на стену", "Вещи которые кладут в карман",
+    "Вещи, которые обычно зеленые", "Вещи, которые обычно красные", "Детские вещи",
+    "Круглые вещи", "Липкие вещи", "Любовь - какая?", "Романтические вещи",
+    "Свадебные вещи"
+];
 
 let possibleAnswers = {
-    // Fill this with your answers
+    // Fill this with your answers for each category and letter
 };
 
-let currentCategory;
+let currentCategories = [];
 let currentLetter;
 let timer;
 let timeRemaining = 60;
@@ -23,12 +46,15 @@ function nextRound() {
     timeRemaining = 60;
     document.getElementById("time").innerText = timeRemaining;
 
-    currentCategory = getRandomCategory();
-    currentLetter = getRandomLetter(currentCategory);
+    currentCategories = getRandomCategories();
+    currentLetter = getRandomLetter(currentCategories);
 
-    document.getElementById("category").innerText = currentCategory;
+    document.getElementById("categories").innerText = currentCategories.join(", ");
     document.getElementById("letter").innerText = currentLetter;
-    document.getElementById("answers").innerText = possibleAnswers[currentCategory][currentLetter].join(", ");
+
+    // Display possible answers for the selected letter and categories
+    const answers = currentCategories.map(cat => possibleAnswers[cat][currentLetter] || []).flat();
+    document.getElementById("answers").innerText = answers.join(", ");
 
     startTimer();
 }
@@ -45,11 +71,20 @@ function startTimer() {
     }, 1000);
 }
 
-function getRandomCategory() {
-    return categories[Math.floor(Math.random() * categories.length)];
+function getRandomCategories() {
+    const shuffled = categories.sort(() => 0.5 - Math.random());
+    return shuffled.slice(0, 5);
 }
 
-function getRandomLetter(category) {
-    const letters = Object.keys(possibleAnswers[category]);
-    return letters[Math.floor(Math.random() * letters.length)];
+function getRandomLetter(categories) {
+    const letters = new Set();
+    categories.forEach(category => {
+        Object.keys(possibleAnswers[category]).forEach(letter => {
+            if (possibleAnswers[category][letter].length) {
+                letters.add(letter);
+            }
+        });
+    });
+    const letterArray = Array.from(letters);
+    return letterArray[Math.floor(Math.random() * letterArray.length)];
 }
